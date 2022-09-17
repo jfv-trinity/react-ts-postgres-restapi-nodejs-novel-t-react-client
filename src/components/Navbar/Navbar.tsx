12e9829e158c85c3d32545f8d1cae5f6cc1d63dc
Joseph <JosephVolpe@outlook.com>
2022-09-17 05:04:50 -0400

    Navbar switches between anon and signed in user

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Navbar.module.scss";
 import { AccountMenu } from "./AccountMenu";
+import { AnonAccountMenu } from "./AnonAccountMenu";
+import { UserContext } from "../../static/UserContext";
 
 function Navbar() {
+  const user = React.useContext(UserContext);
   return (
     <React.Fragment>
       <nav className="navbar">
         <div className="navbar-container">
-          <AccountMenu />
+          {user?.isLoggedIn ? <AccountMenu /> : <AnonAccountMenu />}
         </div>
         <div className="collapse navbar-collapse" id="navbar"></div>
       </nav>
